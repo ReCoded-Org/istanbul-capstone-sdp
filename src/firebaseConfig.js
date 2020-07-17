e019d65142979c3521d2f1cd16230b6b208114da
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-07-18 01:10:06 +0300

    added firebase config.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDkskLw6jF_OUQdnT_qIjm9RMFv1UZsf90",
+    authDomain: "istanbul-capstone-sdp.firebaseapp.com",
+    databaseURL: "https://istanbul-capstone-sdp.firebaseio.com",
+    projectId: "istanbul-capstone-sdp",
+    storageBucket: "istanbul-capstone-sdp.appspot.com",
+    messagingSenderId: "1063575986913",
+    appId: "1:1063575986913:web:3bdafb89139dc5fc9c4da0",
+    measurementId: "G-0JTC6QVEMJ"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;
