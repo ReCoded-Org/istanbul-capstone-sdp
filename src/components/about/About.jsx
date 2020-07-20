ced3e745f51e81f130b313cbc0168e9b46568902
Bilge <lagala999@gmail.com>
2020-07-20 16:26:38 +0300

    implement i18next, create lang dropdown without styling

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./index.css";
+
+function About() {
+    const { t, i18n } = useTranslation();
+    return (
+    );
+}
+
+export default About;
