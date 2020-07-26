cb5b381b344520acc2e825a0ea473d9ba37dd9df
basil-kawak <basilalisa0@gmail.com>
2020-07-26 16:39:01 +0300

    add related carsds

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RelatedCards from './sections/RelatedCards';
+import './index.css';
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <RelatedCards />
+        </div>
+    );
+}
+
+export default Home;
