01aa3b1a784c80fd494397bc447cc3ce91b7cd3d
samah_mustafa <i.samaaah@gmail.com>
2020-07-27 02:31:36 +0300

    working on donation page

---
+++ src/containers/donation/sections/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../index.css";
+
+
+export const Header = () => {
+  return (
+    <div>
+      <img
+        className="header"
+        src={"https://i.ibb.co/hRfR5Mr/Donation-Header.png"}
+        alt="Donation page header"
+      />
+    </div>
+  );
+};
