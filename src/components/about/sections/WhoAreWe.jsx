84614543036a9e08f702aa85b8c6dbb3824c21c0
samah_mustafa <i.samaaah@gmail.com>
2020-07-27 06:54:07 +0300

    fixing WhoAreWe file

--- src/components/about/sections/WhoAreWe.jsx
+++ src/components/about/sections/WhoAreWe.jsx
@@ -1,11 +1,12 @@
-// import React from "react";
-// import { useTranslation } from "react-i18next";
-// import "../index.css";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "../index.css";
 
-// function WhoAreWe() {
-//     const { t, i18n } = useTranslation();
-//     return (
-//     );
-// }
+export const WhoAreWe = () => {
+    const { t, i18n } = useTranslation();
+    return (
+        <div></div>
+    );
+}
 
-// export default WhoAreWe;
+
