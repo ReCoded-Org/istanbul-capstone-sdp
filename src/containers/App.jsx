4a501f64ef00d73a4ca48d0bc89f8646a42c4a22
samah_mustafa <i.samaaah@gmail.com>
2020-07-28 04:51:08 +0300

    some changes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+// import LangDropdown from "../components/LangDropdown";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { Home } from "../components/home/Home";
 import { About } from "../components/about/About";
