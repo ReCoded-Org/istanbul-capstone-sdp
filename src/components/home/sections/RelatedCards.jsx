0323b51dd6a638acf8a71986feb387a874ca52c2
basil-kawak <basilalisa0@gmail.com>
2020-07-28 22:01:27 +0300

    add logos and read more buttons

--- src/components/home/sections/RelatedCards.jsx
+++ src/components/home/sections/RelatedCards.jsx
@@ -1,58 +1,67 @@
-import React from 'react';
-import { CardDeck, Card, Button } from 'react-bootstrap';
+import React from "react";
+import { CardDeck, Card } from "react-bootstrap";
+import logo1 from "../related-cards-logos/Logo1.png";
+import logo2 from "../related-cards-logos/Logo2.png";
+import logo3 from "../related-cards-logos/Logo3.png";
+import logo4 from "../related-cards-logos/Logo4.png";
 
 const RelatedCards = (props) => {
-    return (
-        <div className="cardsContainer">
-            <CardDeck>
-                <Card className="card">
-                    <Card.Img variant="top" src="../300px-Let_Me_In.jpg" />
-                    <Card.Body>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
+  return (
+    <div className="relatedCardsContainer">
+      <CardDeck>
+        <Card>
+          <Card.Body>
+            <Card.Img variant="top" src={logo1} />
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </Card.Text>
+            <a href="#!" className="readMoreCardBtn">
+              Read more >>
+            </a>
+          </Card.Body>
+        </Card>
 
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                    </Card.Body>
-                    <Button variant="outline-light">Light</Button>{' '}
+        <Card>
+          <Card.Body>
+            <Card.Img variant="top" id="scondAndThirdCardLogo" src={logo2} />
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </Card.Text>
+            <a href="#!" className="readMoreCardBtn">
+              Read more >>
+            </a>
+          </Card.Body>
+        </Card>
 
-
-                </Card>
-
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                    </Card.Body>
-                    
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                    </Card.Body>
-                    
-                </Card>
-            </CardDeck>
-
-        </div>
-    )
-}
+        <Card>
+          <Card.Body>
+            <Card.Img id="scondAndThirdCardLogo" src={logo3} />
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </Card.Text>
+            <a href="#!" className="readMoreCardBtn">
+              Read more >>
+            </a>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Img variant="top" src={logo4} />
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </Card.Text>
+            <a href="#!" className="readMoreCardBtn">
+              Read more >>
+            </a>
+          </Card.Body>
+        </Card>
+      </CardDeck>
+    </div>
+  );
+};
 
 export default RelatedCards;
