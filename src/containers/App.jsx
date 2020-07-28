8a5111f0c32fcc90a2f7b6e633ddbf5f8315b0e9
basil-kawak <basilalisa0@gmail.com>
2020-07-28 22:03:10 +0300

    remove comment

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import './index.css';
-import LangDropdown from "../components/LangDropdown"
+import React from "react";
+import "./index.css";
+import LangDropdown from "../components/LangDropdown";
 import { useTranslation } from "react-i18next";
-//components
-import Home from '../components/home/Home'
-
+import Home from "../components/home/Home";
 
 function App() {
   const { t, i18n } = useTranslation();
   return (
     <div className="App">
-      {t('Welcome.1')}
-      <LangDropdown/>
+      {t("Welcome.1")}
+      <LangDropdown />
       <Home />
     </div>
   );
