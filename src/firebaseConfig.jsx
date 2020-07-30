f4cb1e0ee57cb1fa71b67cd3564d4d54094725bf
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-07-30 07:45:57 +0300

    initialized redux and firebase configurations

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,19 +1,20 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDkskLw6jF_OUQdnT_qIjm9RMFv1UZsf90",
-  authDomain: "istanbul-capstone-sdp.firebaseapp.com",
-  databaseURL: "https://istanbul-capstone-sdp.firebaseio.com",
-  projectId: "istanbul-capstone-sdp",
-  storageBucket: "istanbul-capstone-sdp.appspot.com",
-  messagingSenderId: "1063575986913",
-  appId: "1:1063575986913:web:3bdafb89139dc5fc9c4da0",
-  measurementId: "G-0JTC6QVEMJ",
+    apiKey: "AIzaSyDkskLw6jF_OUQdnT_qIjm9RMFv1UZsf90",
+    authDomain: "istanbul-capstone-sdp.firebaseapp.com",
+    databaseURL: "https://istanbul-capstone-sdp.firebaseio.com",
+    projectId: "istanbul-capstone-sdp",
+    storageBucket: "istanbul-capstone-sdp.appspot.com",
+    messagingSenderId: "1063575986913",
+    appId: "1:1063575986913:web:3bdafb89139dc5fc9c4da0",
+    measurementId: "G-0JTC6QVEMJ"
 };
 
 // Initialize Firebase
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+firebase.initializeApp(firebaseConfig);
 
-export default db;
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,66 @@ import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "./i18n";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/rootReducer';
+import { Provider, useSelector } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase, isLoaded } from 'react-redux-firebase';
+import firebaseConfig from './firebaseConfig';
+import firebase from 'firebase/app';
+import { Container } from 'react-bootstrap';
+
+
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase, firebaseConfig)
+  )
+);
+
+//construct required properties
+const profileSpecificProps = {
+  userProfile: 'profiles',
+  useFirestoreForProfile: true,
+  enableRedirectHandling: false,
+  resetBeforeLogin: false
+}
+
+//second config propery added
+const rrfProps = {
+  firebase,
+
+  config: firebaseConfig,
+  //second config added here
+  config: profileSpecificProps,
+
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
+
+const AuthIsLoaded = ({ children }) => {
+  const auth = useSelector( state => state.firebase.auth );
+  if(!isLoaded(auth)) {
+    return <Container><h4>Loading...</h4></Container>
+  }
+  return children;
+}
+
 
 ReactDOM.render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <App />
-  </Suspense>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <AuthIsLoaded>
+          <Suspense fallback={<div>Loading...</div>}>
+            <App />
+          </Suspense>
+        </AuthIsLoaded>
+      </ ReactReduxFirebaseProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
