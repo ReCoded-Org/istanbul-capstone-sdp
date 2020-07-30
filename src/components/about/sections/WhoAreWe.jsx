24db5afb0b81f1228be8aa592c1d208c2e3c76ae
basil-kawak <basilalisa0@gmail.com>
2020-07-30 22:21:50 +0300

    merge dev into temp

--- src/components/about/sections/WhoAreWe.jsx
+++ src/components/about/sections/WhoAreWe.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import "../index.css";
 
 function WhoAreWe() {
-    const { t, i18n } = useTranslation();
-    return (
-    );
+  //   const { t, i18n } = useTranslation();
+  return <div></div>;
 }
 
 export default WhoAreWe;
