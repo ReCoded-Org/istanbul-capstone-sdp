ca4a66e22fd72681f23ea370953e37392894b19e
Mahir-Süleyman <57353113+maher-suleyman@users.noreply.github.com>
2020-07-31 13:58:11 +0300

    Update blogReducer.jsx

--- src/reducers/blogReducer.jsx
+++ src/reducers/blogReducer.jsx
@@ -4,8 +4,10 @@ const blogReducer = (state = initState, action) => {
   switch (action.type) {
     case "ADD_BLOG":
       console.log("a new blog has been added");
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
 
 export default blogReducer;
