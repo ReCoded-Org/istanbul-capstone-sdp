5b262a56e52a88b8960343cdaeb1c5f59927e0ba
samah_mustafa <i.samaaah@gmail.com>
2020-08-05 00:21:30 +0300

    adding the header

---
+++ src/components/singleBolgPage/sections/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Donation = () => {
+  return (
+    <div>
+      <img
+        className="header"
+        src={"https://i.ibb.co/zmknvj9/single-post-header.png"}
+        alt="Donation page header"
+      />
+    </div>
+  );
+  
+};
