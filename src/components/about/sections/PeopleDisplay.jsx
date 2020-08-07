49c80b3a2e38b480ef1f71fa1fd8f59554d8a8fb
Bilge <lagala999@gmail.com>
2020-08-07 09:43:51 +0300

    fix translation files, delete unnecess vars, fix alt text

--- src/components/about/sections/PeopleDisplay.jsx
+++ src/components/about/sections/PeopleDisplay.jsx
@@ -3,38 +3,41 @@ import "../index.css";
 import PersonDisplay from "./PersonDisplay";
 import { Row } from "react-bootstrap";
 
-// i18n translations might still be loaded by the http backend
-// use react's Suspense
+const STAFF_SPECS = [
+  {
+    name: "nasser",
+    title: "managerAssistant",
+    photo: require("././images/staff0.png"),
+    index: 0,
+  },
+  {
+    name: "jawdat",
+    title: "generalManager",
+    photo: require("././images/staff1.png"),
+    index: 1,
+  },
+  {
+    name: "turgut",
+    title: "psychotherapist",
+    photo: require("././images/staff2.png"),
+    index: 2,
+  },
+];
 
 export default function peopleDisplay() {
-  const peopleData = [
-    {
-      name: "nasser",
-      index: 0,
-    },
-    {
-      name: "jawdat",
-      index: 1,
-    },
-    {
-      name: "turgut",
-      index: 2,
-    },
-  ];
-
-  const renderPeople = (peopleDataList = peopleData) => {
-    const renderElements = peopleDataList.map((person, i) => {
-      return (
-        <PersonDisplay
-          name={person.name}
-          photoLink={person.photoLink}
-          index={person.index}
-          key={i}
-        />
-      );
-    });
-    return renderElements;
-  };
-
-  return <Row>{renderPeople(peopleData)}</Row>;
+  return (
+    <Row>
+      {STAFF_SPECS.map((staff, i) => {
+        return (
+          <PersonDisplay
+            name={staff.name}
+            photo={staff.photo}
+            translationKey={staff.title}
+            index={staff.index}
+            key={i}
+          />
+        );
+      })}
+    </Row>
+  );
 }
