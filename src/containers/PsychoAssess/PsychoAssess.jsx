2cc260284bd1f901e242a7c18cab23fa66cf37db
Bilge <lagala999@gmail.com>
2020-08-11 14:02:58 +0300

    create test structure, content

---
+++ src/containers/PsychoAssess/PsychoAssess.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./index.css";
+import Questions from "./sections/Questions";
+
+export default function PsychoAssess() {
+  return (
+    <Container>
+      <Questions />
+    </Container>
+  );
+}
