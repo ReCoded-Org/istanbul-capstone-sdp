c3e0d450733f738a0ffe87120aaad51943fb02e3
Tugba Sen <ttugbasen@gmail.com>
2020-08-11 23:07:30 +0300

    add sign-in and sign up components

---
+++ src/components/signInUp/SignInUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import SignIn from "./sections/SignIn";
+import SignUp from "./sections/SignUp";
+
+const SignInUp = () => {
+  return (
+    <div>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignInUp;
---
+++ src/components/signInUp/sections/SignUp.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./index.css";
+import EmailPasswordSignUp from "../../../containers/auth/EmailPasswordSignUp";
+import FacebookAuth from "../../../containers/auth/FacebookAuth";
+import GoogleAuth from "../../../containers/auth/GoogleAuth";
+import { Row, Col } from "react-bootstrap";
+
+const SignUp = (props) => {
+  return (
+    <Row className="signinUpContainer justify-content-md-center">
+      <Col className="d-flex mt-5 mb-5 shadow-lg rounded p-0 " lg="7" sm="12">
+        <Col lg="5" sm="5" className="rightSection">
+          <p>need help?</p>
+          <h1>
+            Welcome <br /> to our <br /> website.
+          </h1>
+        </Col>
+
+        <Col lg="7" sm="7" className="leftSection">
+          <h2>sign up</h2>
+          <EmailPasswordSignUp />
+          <Row className="m-2 ">
+            <h5>Or SignUp using </h5>
+            <FacebookAuth />
+            <GoogleAuth />
+          </Row>
+
+          <h6>
+            Already a member?{" "}
+            <span onClick={props.handleLoggingSwitch}>Login</span>
+          </h6>
+        </Col>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUp;
