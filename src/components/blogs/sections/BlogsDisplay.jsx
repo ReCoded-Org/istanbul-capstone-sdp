ef74da1bb23edcd74e789e4d2b6c6cc166f8c862
Bilge <lagala999@gmail.com>
2020-08-13 13:38:03 +0300

    change camelCase, move constant vars to outside

--- src/components/blogs/sections/BlogsDisplay.jsx
+++ src/components/blogs/sections/BlogsDisplay.jsx
@@ -3,25 +3,26 @@ import BlogDisplay from "./BlogDisplay";
 import { Link } from "react-router-dom";
 import "../index.css";
 
+const BLOG_POST_SPECS = [
+  {
+    photo: require("././images/blog0.jpg"),
+  },
+  {
+    photo: require("././images/blog1.jpg"),
+  },
+  {
+    photo: require("././images/blog2.jpg"),
+  },
+  {
+    photo: require("././images/blog3.jpg"),
+  },
+];
+
 export default function BlogsDisplay() {
-  const BLOGPOST_SPECS = [
-    {
-      photo: require("././images/blog0.jpg"),
-    },
-    {
-      photo: require("././images/blog1.jpg"),
-    },
-    {
-      photo: require("././images/blog2.jpg"),
-    },
-    {
-      photo: require("././images/blog3.jpg"),
-    },
-  ];
   // since we're using arrays in i18next, its index will be translation key
   return (
     <>
-      {BLOGPOST_SPECS.map((blogpost, i) => {
+      {BLOG_POST_SPECS.map((blogpost, i) => {
         return (
           <Link to={`/blogposts/${i}`} key={`blogPost${i}`}>
             <BlogDisplay translationKey={i} photo={blogpost.photo} />
