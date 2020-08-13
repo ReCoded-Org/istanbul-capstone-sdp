faf80798db0d06a077725afddb0698127c1fcf40
samah_mustafa <i.samaaah@gmail.com>
2020-08-13 23:38:12 +0300

    returning line removed by mistake

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import { Header } from "./sections/Header";
 import RelatedCards from "./sections/RelatedCards";
 import LiveChat from "../../containers/liveChat/LiveChat";
