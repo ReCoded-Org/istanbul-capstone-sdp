2b041ea912a443f9f15c8c426842e036e08a2e79
basil-kawak <basilalisa0@gmail.com>
2020-08-13 05:27:16 +0300

    include live chat in home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Header } from "./sections/Header";
 import "./index.css";
 import RelatedCards from "./sections/RelatedCards";
+import LiveChat from "../../containers/LiveChat";
 
 export const Home = () => {
   return (
     <div>
+      <LiveChat />
       <Header />
       <RelatedCards />
     </div>
