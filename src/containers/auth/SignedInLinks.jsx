a958a70127e36f44c6b34f12d64d5e73bfb65bf4
basil-kawak <basilalisa0@gmail.com>
2020-08-14 15:53:54 +0300

    fix profile path

--- src/containers/auth/SignedInLinks.jsx
+++ src/containers/auth/SignedInLinks.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { useTranslation } from "react-i18next";
-import { signOut } from "../../../actions/authActions";
+import { signOut } from "../../actions/authActions";
 import "./index.css";
 
 const SignedInLinks = (props) => {
