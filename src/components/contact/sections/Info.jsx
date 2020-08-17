fda1f61e40873d29702f18d8cdf398f264b8b6b9
Bilge <lagala999@gmail.com>
2020-08-18 00:13:17 +0300

    translate contact us to tr

--- src/components/contact/sections/Info.jsx
+++ src/components/contact/sections/Info.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { Container, Col, Nav } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 const Info = () => {
+  const { t } = useTranslation();
+
   return (
     <Container>
       <Col className="infoSection">
-        <Nav.Link href="#">CONTACTS:</Nav.Link>
+        <Nav.Link href="#">{t("contact.info.0")}</Nav.Link>
         <p>
           (+90) 533-382-094 <br />
           suljak@suljak.com <br />
-          43 Foundry Avenue, Basaksehir, istotc, Istanbul, Turkey
+          {t("contact.info.1")}
         </p>
       </Col>
       <Col className="infoSection">
-        <Nav.Link href="#">BUSINESS HOURS:</Nav.Link>
+        <Nav.Link href="#">{t("contact.info.2")}</Nav.Link>
         <p>
-          Our support hotline is available 24 hours a day: 11401 <br />
-          Monday-Friday: 5pm to 9pm <br />
-          Saturday: 10am to 2pm <br />
-          Sunday: Closed
+          {t("contact.info.3")} <br />
+          {t("contact.info.4")}
+          <br />
+          {t("contact.info.5")} <br />
+          {t("contact.info.6")}
         </p>
       </Col>
     </Container>
