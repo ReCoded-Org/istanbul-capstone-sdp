38492d1d7f7d3e84210d9e45405537f14bfab503
samah-mustafa <i.samaaah@gmail.com>
2020-08-18 02:46:59 +0300

    headers and footers for all pages

--- src/containers/PsychoAssess/PsychoAssess.jsx
+++ src/containers/PsychoAssess/PsychoAssess.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "./index.css";
 import Questions from "./sections/Questions";
+import { Header } from "./sections/Header";
+import Footer from "../../components/common/Footer";
 
 export default function PsychoAssess() {
   return (
+    <div>
+    <Header />
     <Container>
       <Questions />
     </Container>
+    <Footer />
+    </div>
   );
 }
