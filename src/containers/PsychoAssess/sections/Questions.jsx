cb990ce2ec5c7ca639039406907b7918ae03f343
Bilge <lagala999@gmail.com>
2020-08-17 16:26:50 +0300

    add point system, modals to show result, change wrong links at homepage

--- src/containers/PsychoAssess/sections/Questions.jsx
+++ src/containers/PsychoAssess/sections/Questions.jsx
@@ -1,15 +1,120 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Col, Row, Button, Form } from "react-bootstrap";
+import { Col, Row, Button, Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "../index.css";
-import { useState } from "react";
 
 const OPTION_QUANTITY = 4;
 const QUESTION_QUANTITY = 13;
 
 export default function Questions() {
-  const [isSubmitted, setSubmitted] = useState(false);
+  const [submitModalShow, setSubmitModalShow] = React.useState(false);
   const { t } = useTranslation();
+
+  const SubmitModal = (props) => {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {t("assessment.title")}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{showResults(calculateScore())}</Modal.Body>
+        <Modal.Footer>
+          <Link to="/contact">
+            <Button className="testContactButton" variant="primary">
+              {t("assessment.contactButton")}
+            </Button>
+          </Link>
+          <Button className="testDoneButton" onClick={props.onHide}>
+            {t("assessment.closeButton")}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
+  const showResults = (score) => {
+    //
+    console.log(score);
+    if (score !== -15) {
+      if (score >= 52) {
+        return (
+          <>
+            <span className="resultCategory">
+              {t("assessment.verySevere.2")}
+            </span>
+            <h3>{t("assessment.verySevere.0")}</h3>
+            <h6>{t("assessment.submitSuccess")}</h6>
+            <h4>{t("assessment.verySevere.1")}</h4>
+          </>
+        );
+      } else if (score >= 25) {
+        return (
+          <>
+            <span className="resultCategory">{t("assessment.severe.2")}</span>
+            <h5>{t("assessment.severe.0")}</h5>
+            <h6>{t("assessment.submitSuccess")}</h6>
+            <p>{t("assessment.severe.1")}</p>
+            <Link to="/contact">
+              <Button className="testSubmitButton" variant="primary">
+                {t("assessment.contactButton")}
+              </Button>
+            </Link>
+          </>
+        );
+      } else if (score >= 18) {
+        return (
+          <>
+            <span className="resultCategory">{t("assessment.moderate.2")}</span>
+            <h5>{t("assessment.moderate.0")}</h5>
+            <h6>{t("assessment.submitSuccess")}</h6>
+            <p>{t("assessment.moderate.1")}</p>
+          </>
+        );
+      } else if (score >= 13) {
+        return (
+          <>
+            <span className="resultCategory">{t("assessment.mild.2")}</span>
+            <h5>{t("assessment.mild.0")}</h5>
+            <h6>{t("assessment.submitSuccess")}</h6>
+            <p>{t("assessment.mild.1")}</p>
+          </>
+        );
+      } else if (score >= 7) {
+        return (
+          <>
+            <span className="resultCategory">{t("assessment.light.2")}</span>
+            <h5>{t("assessment.light.0")}</h5>
+            <h6>{t("assessment.submitSuccess")}</h6>
+            <p>{t("assessment.light.1")}</p>
+          </>
+        );
+      }
+    }
+    return <h5>{t("assessment.answerMissing")}</h5>;
+  };
+
+  let userScore = 0;
+  const calculateScore = () => {
+    const radioButtons = document.getElementsByClassName("testChoice");
+    let checkedRadioNumber = 0;
+    for (let i = 0; i < radioButtons.length; i++) {
+      if (radioButtons.item(i).checked) {
+        userScore += parseInt(radioButtons.item(i).value);
+        checkedRadioNumber++;
+      }
+    }
+    if (checkedRadioNumber === QUESTION_QUANTITY) {
+      return userScore;
+    }
+    return -15;
+  };
 
   let questionCount = 0;
   const renderQuestions = () => {
@@ -36,10 +141,10 @@ export default function Questions() {
       options.push(
         <li key={`question${questionCount}.${i}`} className="form-check">
           <input
-            className="form-check-input"
+            className="form-check-input testChoice"
             type="radio"
             name={`question${questionCount}`}
-            value="0"
+            value={`${i}`}
           />
           <label className="form-check-label" htmlFor="exampleRadios1">
             {t(`questions.${questionCount}.multiple.${i}`)}
@@ -59,27 +164,20 @@ export default function Questions() {
         </div>
         {renderQuestions()}
         <Row className="testSubmitButtonRow">
-          <Form.Label>{t("testSubmit.email")}</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">{t("testSubmit.email")}</Form.Text>
           <Button
             variant="primary"
             className="testSubmitButton"
             onClick={() => {
-              setSubmitted(true);
-              setInterval(() => {
-                setSubmitted(false);
-              }, 3000);
+              setSubmitModalShow(true);
             }}
           >
             Submit
           </Button>
         </Row>
-        {isSubmitted && (
-          <div class="alert alert-primary submitAlert" role="alert">
-            {t("testSubmit.message")}
-          </div>
-        )}
+        <SubmitModal
+          show={submitModalShow}
+          onHide={() => setSubmitModalShow(false)}
+        />
       </Col>
     </>
   );
