4ab0601833e86c84a9546bac6a4a3d10b585c79c
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-08-18 10:43:39 +0300

    debugging

--- src/containers/auth/LoggingMenu.jsx
+++ src/containers/auth/LoggingMenu.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import {
-  Container,
   Image,
-  Button,
   Dropdown,
-  Toggle,
-  Menu,
-  Item,
-  DropdownButton,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import "./index.css";
