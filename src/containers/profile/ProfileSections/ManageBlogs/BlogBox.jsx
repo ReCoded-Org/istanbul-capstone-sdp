ac6b7658492c19b5736faa52ea6aec8bb40f2c73
basil-kawak <basilalisa0@gmail.com>
2020-08-18 08:50:39 +0300

    add manage blog style

---
+++ src/containers/profile/ProfileSections/ManageBlogs/BlogBox.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { updateProfile } from "../../../../actions/authActions";
+import { Dropdown, Button, Row, Col, ListGroup, Image } from "react-bootstrap";
+import "./BlogBox.css";
+import anonymousImage from "../../../../images/anonymousImage.png";
+
+const ADMIN_ROLE = "ADMIN";
+
+const BlogBox = (props) => {
+  const { blog, userType } = props;
+
+  const photoSrc = blog.authorProfileImage || anonymousImage;
+
+  const handleBlockAndActivateButton = () => {
+    if (userType === ADMIN_ROLE) {
+      if (blog.approvement) {
+        return (
+          <Button
+            variant="danger"
+            className="w-75"
+            onClick={() =>
+              props.approveBlog({ blogId: blog.blogId, approvement: false })
+            }
+          >
+            Decline
+          </Button>
+        );
+      } else {
+        return (
+          <Button
+            variant="success"
+            className="w-75"
+            onClick={() =>
+              props.approveBlog({ blogId: blog.blogId, approvement: true })
+            }
+          >
+            Approve
+          </Button>
+        );
+      }
+    } else {
+      return (
+        <>
+          <Button
+            variant="success"
+            className="w-75 mr-2"
+            onClick={() =>
+              // props.updateProfile({ userId: profile.id, isBlocked: false })
+              {}
+            }
+          >
+            Edit
+          </Button>
+          <Button
+            variant="dancer"
+            className="w-75"
+            onClick={() =>
+              // props.updateProfile({ userId: profile.id, isBlocked: false })
+              {}
+            }
+          >
+            Delete
+          </Button>
+        </>
+      );
+    }
+  };
+
+  return (
+    <div className="blogBox">
+      <Row>
+        <Col xs={5}>
+          <Link to={"/profiles/" + blog.userId} key={blog.userId}>
+            <ListGroup defaultActiveKey="none">
+              <ListGroup.Item action className="d-flex">
+                <Image
+                  src={photoSrc}
+                  alt="Profile"
+                  roundedCircle
+                  width="100"
+                  height="100"
+                  className="mr-3"
+                />
+                <div class="blogBoxInfo">
+                  <h5>{blog.author}</h5>
+                  <p>
+                    {blog.authorJobTitle
+                      ? blog.authorJobTitle
+                      : "No job title available"}
+                  </p>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Link>
+        </Col>
+
+        <Col xs={5}>
+          <Link to={"/blogs/" + blog.blogId} key={blog.blogId}>
+            <ListGroup defaultActiveKey="none">
+              <ListGroup.Item action className="d-flex">
+                <Image
+                  src={blog.imageURL}
+                  alt="Profile"
+                  thumbnail
+                  width="100"
+                  className="mr-3"
+                />
+                <div class="blogBoxInfo">
+                  <h5>{blog.title}</h5>
+                  <p>{blog.date}</p>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Link>
+        </Col>
+
+        <Col
+          xs={2}
+          className="d-flex flex-column pt-2 pb-2 justify-content-around"
+        >
+          {handleBlockAndActivateButton()}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateProfile: (profileInfo) => dispatch(updateProfile(profileInfo)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(BlogBox);
--- src/containers/profile/ProfileSections/ManageBlogs/ManageBlogs.jsx
+++ src/containers/profile/ProfileSections/ManageBlogs/ManageBlogs.jsx
@@ -1,7 +1,93 @@
 import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { Button, ListGroup } from "react-bootstrap";
+import BlogEditor from "../../../../containers/BlogEditor/BlogEditor";
+import BlogBox from "./BlogBox";
+import { approveBlog } from "../../../../actions/blogActions";
 
-const ManageBlogs = () => {
-  return <div>MANAGE BLOGS UNDER CONSTRUCTION</div>;
+const ADMIN_ROLE = "ADMIN";
+const AUTHOR_ROLE = "AUTHOR";
+
+const ManageBlogs = (props) => {
+  const { allBlogs, userId, currentUserBlogs, profile } = props;
+
+  const [newBlog, setNewBlog] = React.useState(null);
+
+  const closeBlogEditor = () => {
+    setNewBlog(null);
+  };
+
+  const displayedBlogs =
+    profile.userType === ADMIN_ROLE ? allBlogs : currentUserBlogs;
+
+  return (
+    <ListGroup defaultActiveKey="none">
+      {profile.userType === AUTHOR_ROLE && (
+        <Button
+          variant="success"
+          className="mb-5"
+          onClick={() =>
+            setNewBlog(
+              <BlogEditor closeBlogEditor={closeBlogEditor} profile={profile} />
+            )
+          }
+        >
+          Add a new blog
+        </Button>
+      )}
+
+      {newBlog}
+
+      {displayedBlogs &&
+        !newBlog &&
+        displayedBlogs.map((blog) => {
+          return (
+            <ListGroup.Item>
+              <BlogBox
+                approveBlog={props.approveBlog}
+                blog={blog}
+                userType={profile.userType}
+              />
+            </ListGroup.Item>
+          );
+        })}
+    </ListGroup>
+  );
 };
 
-export default ManageBlogs;
+const mapStateToProps = (state, ownProps) => {
+  const { userId } = ownProps;
+  const blogs = state.firestore.data.blogs;
+
+  const allBlogs = [];
+
+  const currentUserBlogs = [];
+  if (blogs) {
+    for (const blogId in blogs) {
+      if (blogs[blogId].userId === userId) {
+        currentUserBlogs.push(blogs[blogId]);
+      }
+      allBlogs.push(blogs[blogId]);
+    }
+  }
+
+  return {
+    currentUserBlogs,
+    allBlogs,
+    auth: state.firebase.auth,
+    userId,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    approveBlog: (blogData) => dispatch(approveBlog(blogData)),
+  };
+};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect([{ collection: "blogs" }])
+)(ManageBlogs);
